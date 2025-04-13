955799d28f7008640b1b82b262713e5d32d82bab
fattpanduh <bamaboydreek@gmail.com>
2025-04-13 18:29:06 -0400

    Version 1: Added DNA Strand

---
+++ DNA Animation.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+import random, sys, time
+
+PAUSE = 0.15 # (!) Try changing this to 0.5 or 0.0.
+
+# These are the individual rows of the DNA animation:
+ROWS = [
+    #123456789 <- Use this to measure the number of spaces:
+    '         ##' , # Index 0 has no {}.
+    '        #{}-{}#' ,
+    '       #{}---{}#' ,
+    '      #{}-----{}#' ,
+    '     #{}------{}#' ,
+    '    #{}------{}#',
+    '    #{}-----{}#',
+    '     #{}---{}#',
+    '     #{}-{}#',
+    '      ##', # Index 9 has no {}
+    '     #{}-{}#',
+    '     #{}---{}#',
+    '    #{}-----{}#',
+    '    #{}------{}#',
+    '     #{}------{}#',
+    '      #{}-----{}#',
+    '       #{}---{}#',
+    '        #{}-{}#']
+
+
+
+
+
+
+]
